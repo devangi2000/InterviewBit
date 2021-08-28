b3f21c1cce83c559fa92c576fbf8f8bcd17abf31
Devangi Purkayastha <54393816+devangi2000@users.noreply.github.com>
2021-08-28 22:58:01 +0530

    ...

--- Arrays/Sorting/MaxDistance.cpp
+++ Arrays/Sorting/MaxDistance.cpp
@@ -1,24 +1,17 @@
-// Problem Description
 // Given an array A of integers, find the maximum of j - i 
 // subjected to the constraint of A[i] <= A[j].
-
 // Input Format
 // First and only argument is an integer array A.
-
 // Output Format
 // Return an integer denoting the maximum value of j - i;
-
 // Example Input
 // Input 1:
 //  A = [3, 5, 4, 2]
-
 // Example Output
 // Output 1:
 //  2
-
 // Example Explanation
 // Explanation 1:
-
 //  Maximum value occurs for pair (3, 4).
 
 int Solution::maximumGap(const vector<int> &A) {
